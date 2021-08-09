629e7e9be253c7f7e41b630f6c489ffbe7126838
Anton Mokrousov <settingx@mail.ru>
2021-08-10 02:39:22 +0700

    Artyom's pragmas

--- template.cpp
+++ template.cpp
@@ -1,4 +1,12 @@
 #include <bits/stdc++.h>
+
+#pragma optimization_level 3
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize("Ofast")//Comment optimisations for interactive problems (use endl)
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+
 using namespace std;
 #ifdef ILIKEGENTOO
 void E(){}template<class A,class...B>void E(A $,B..._){cerr<<' '<<$;E(_...);}
@@ -11,12 +19,14 @@ void E(){}template<class A,class...B>void E(A $,B..._){cerr<<' '<<$;E(_...);}
 
 typedef double flt;
 typedef long long int64;
-typedef pair<int, int> pii;
 
-const int inf = 0x3f3f3f3f;
+const int infI = 0x3f3f3f3f;
+const int infLL = 0x3f3f3f3f3f3f3f3f;
 
 int main() {
+	cin.tie(0);
+    cout.tie(0);
     ios_base::sync_with_stdio(false);
-
+	cout << setprecision(15) << fixed;
     return 0;
 }
