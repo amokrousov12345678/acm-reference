bf077163af58e77ea406c610deb3981b5f655c06
Anton Mokrousov <settingx@mail.ru>
2021-09-13 22:45:58 +0700

    FFT + poly inverse + division

--- fft_prime.cpp
+++ fft_prime.cpp
@@ -1,32 +1,42 @@
-// ******************************** FFT ********************************
-typedef complex<double> base;
-int rev[MAXN];
-base wlen_pw[MAXN];
-
-void fft(base a[], int n, bool invert) {
-	for (int i = 0; i < n; ++i)
-		if (i < rev[i]) swap(a[i], a[rev[i]]);
+const int mod = 998244353;//7340033;
+const int root = 15311432 /*3^(7*17)*/ //5;
+const int root_1 = //4404020;
+const int root_pw = //1<<20;
  
-	for (int len = 2; len <= n; len <<= 1) {
-		double ang = 2 * PI / len * (invert ? -1 : +1);
-		int len2 = len >> 1;
+//n MUST BE 2^k
+void fft (int* a, int n, bool invert) {
+	int n = (int) a.size();
  
-		base wlen (cos(ang), sin(ang));
-		wlen_pw[0] = base (1, 0);
-		for (int i = 1; i < len2; ++i) wlen_pw[i] = wlen_pw[i-1] * wlen;
-		for (int i = 0; i < n; i += len) {
-			base t,
-				 *pu = a+i,
-				 *pv = a+i+len2, 
-				 *pu_end = a+i+len2,
-				 *pw = wlen_pw;
-			for ( ; pu != pu_end; ++pu, ++pv, ++pw) {
-				t = *pv * *pw;
-				*pv = *pu - t;
-				*pu += t;
+	for (int i=1, j=0; i<n; ++i) {
+		int bit = n >> 1;
+		for (; j>=bit; bit>>=1)
+			j -= bit;
+		j += bit;
+		if (i < j)
+			swap (a[i], a[j]);
+	}
+ 
+	for (int len=2; len<=n; len<<=1) {
+		int wlen = invert ? root_1 : root;
+		for (int i=len; i<root_pw; i<<=1)
+			wlen = int (wlen * 1ll * wlen % mod);
+		for (int i=0; i<n; i+=len) {
+			int w = 1;
+			for (int j=0; j<len/2; ++j) {
+				int u = a[i+j],  v = int (a[i+j+len/2] * 1ll * w % mod);
+				a[i+j] = u+v < mod ? u+v : u+v-mod;
+				a[i+j+len/2] = u-v >= 0 ? u-v : u-v+mod;
+				w = int (w * 1ll * wlen % mod);
 			}
 		}
 	}
-	if (invert)
-		for (int i = 0; i < n; ++i) a[i] /= n;
+	if (invert) {
+		int nrev = inv(n, mod);
+		for (int i=0; i<n; ++i)
+			a[i] = int (a[i] * 1ll * nrev % mod);
+	}
 }
+
+//inv series: A(x)*Bn(x) = 1 + x^n*C(x): B1 = A(x)[0]^-1, B2k = Bk*(2-A*Bk) (only 2k members)
+//division: deg A = m, degB = n. A = BQ + R, Br, Ar, Qr - polys with reverse coeff order,
+//D - (m+1-n) first of Br. Then Qr = D*A (m-n+1 coeffs, need to reverse)
