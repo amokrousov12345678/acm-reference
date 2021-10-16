236acb50639765e63b64f668d85143b25c4dfe1d
Anton Mokrousov <settingx@mail.ru>
2021-10-16 13:03:35 +0700

    Diofant equations

--- num_theory.cpp
+++ num_theory.cpp
@@ -29,7 +29,8 @@ mu[1] = 1;
 for (int i=2;i<maxn;i++) {
     ll pred = i/lp[i]; mu[i] = pred % lp[i] ? -mu[pred] : 0;
 }
-int get_log(int a, int b, int m) { // discrete log (x, s.t. a^x=b(mod m)
+// discrete log PRIME MODULO (x, s.t. a^x=b(mod m)
+int getLog(int a, int b, int m) { 
     int n = ((int) sqrt (m)) + 1;
     int an = 1; for (int i=0; i<n; ++i) an = mul(an, a, m);
     map<int,int> vals;
@@ -46,13 +47,28 @@ int get_log(int a, int b, int m) { // discrete log (x, s.t. a^x=b(mod m)
     }
     assert(0); return -1;
 }
-
 int factmod(int n, int p) { //NOT CHECKED
-    int res = 1; // factorial by modulo (with thrown pows of p)
+    int res = 1; // factorial by modulo (with discarded pows of p)
     while (n > 1) {
         res = (res * ((n / p) % 2 ? p - 1 : 1)) % p;
         for (int i = 2; i <= n % p; ++i) res = (res * i) % p;
         n /= p;
     }
     return res % p;
+}
+//get x,y, s.t. a*x+b*y = gcd(a,b) (ret. value)
+ll ext_gcd(ll a, ll b, ll& x, ll& y) {
+    if (a == 0) { x = 0; y = 1; return b;}
+    ll x1, y1;
+    ll d = ext_gcd(b%a, a, x1, y1);
+    x = y1 - (b / a) * x1; y = x1;
+    return d;
+}
+//false - no solution, true - series X=x+k*dx, Y=y+k*dy, k in Z
+bool solveDiofant(ll a, ll b, ll c, ll& x, ll& y, ll& dx, ll& dy) {
+    ll _x, _y; ll gcd = ext_gcd(a,b,_x,_y);
+    if (c % gcd) return false;
+    x = _x*(c/gcd); y = _y*(c/gcd);
+    dx = b/abs(gcd); dy = -a/abs(gcd);
+    return true;
 }
