21beb363523191e58f65429885e917a67c7b8433
Anton Mokrousov <settingx@mail.ru>
2021-10-19 17:12:52 +0700

    Graph biconnected components of edges

--- graph_comp.cpp
+++ graph_comp.cpp
@@ -43,56 +43,36 @@ void dfsBridges(int v, int par = -1) {
 		if (low[g[v][i]] > num[v]) isBridge[v][i] = 1;
 	}
 }
-//TBD: write biconnected components in more simple way
-int low[maxn], was[maxn], num[maxn], comp_num[maxn], cur;
-int n;
-vector<int> is_bridge[maxn], back[maxn], comp[maxn];
+//Biconn. component of verts - set of verts, each pair have at least 2 EDGE independent paths
+//Can be found via dfs, when traverse bridge - enter new component
+//Biconn. component of edges - set of edges, each pair have at least 2 VERT independent paths between ends
+//Found by algo below
+vector<int> compId[maxn];//OUT compId[u][id] - id of biconn component of edge g[u][id]
+
+vector<int> back[maxn];
 pair<int, int> stck[maxe];
-int ssize, comp_cnt, is_art[maxn];
-
-void add_biedge(int u, int v) {
-	back[u].push_back(g[v].size());
-	back[v].push_back(g[u].size());
-	g[u].push_back(v);
-	g[v].push_back(u);
-	comp[u].push_back(-1);
-	comp[v].push_back(-1);
-	is_bridge[u].push_back(0);
-	is_bridge[v].push_back(0);
-}
-void clear() {
-	for (int i = 0; i < n; ++i) {
-		g[i].clear();
-		is_bridge[i].clear();
-		back[i].clear();
-		comp[i].clear();
-	}
-	memset(num, -1, sizeof(num));
-	memset(was, 0, sizeof(was));
-	cur = comp_cnt = ssize = 0;
-}
-
-// ***** Searching for biconnected components *****
+int ssize = 0;//MUST be set to 0 before call
 void release(int u, int v) {
-	while (ssize --> 0) {
+	while (ssize-- > 0) {
 		int i = stck[ssize].first, j = stck[ssize].second;
-		comp[i][j] = comp[ g[i][j] ][ back[i][j] ] = comp_cnt;
+		comp[i][j] = comp[g[i][j]][back[i][j]] = comp_cnt;
 		if ((i == u && g[i][j] == v) || (i == v && g[i][j] == u)) break;
 	}
 	comp_cnt++;
 }
-void dfs(int v, int par) {
+void dfs(int v, int par) {//Biconn. component of edges in CONNECTED COMPONENT
+	compId[v].clear(); compId[v].assign(g[v].size(), -1);
 	was[v] = 1;
 	num[v] = low[v] = cur++;
 	for (int i = 0; i < g[v].size(); ++i) {
 		if (g[v][i] == par) continue;
-		if (num[ g[v][i] ] < num[v]) stck[ssize++] = make_pair(v, i);
-		if (was[ g[v][i] ]) {
-			low[v] = min(low[v], num[ g[v][i] ]);
+		if (num[g[v][i]] < num[v]) stck[ssize++] = make_pair(v, i);
+		if (was[g[v][i]]) {
+			low[v] = min(low[v], num[g[v][i]]);
 			continue;
 		}
 		dfs(g[v][i], v);
-		low[v] = min(low[v], low[ g[v][i] ]);
-		if (low[ g[v][i] ] >= num[v]) release(v, g[v][i]);
+		low[v] = min(low[v], low[g[v][i]]);
+		if (low[g[v][i]] >= num[v]) release(v, g[v][i]);
 	}
 }
