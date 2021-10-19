43e4c0015213e9051d122c38d4b583c3c20c0fea
Anton Mokrousov <settingx@mail.ru>
2021-10-19 17:12:28 +0700

    Persistent segtree

--- range_tree.cpp
+++ range_tree.cpp
@@ -81,49 +81,32 @@ struct fenvic_t { //for multiple dimensions loops by each index
 struct segtree {
     val_t mas[4*size]; const static val_t neutral = 0;
     pval_t def[4*size]; const static pval_t neutralUpdate = 0;
-  
-    val_t applyUpdate(val_t val, val_t push) {};
-    val_t rqOp(val_t lhs, val_t rhs) {}
-    val_t combineUpdates(val_t cur, val_t fromUp) {return cur+fromUp;};
+    val_t applyUpdate(val_t val, val_t push) {};//apply update to  vertex
+    val_t rqOp(val_t lhs, val_t rhs) {}//op for range query
+    val_t combineUpdates(val_t cur, val_t fromUp) {return cur+fromUp;};//apply def[par] into def[v]
 
     void build(int* vals, int n, int v = 1, int l = 0, int r = size-1) {
-        if (l==r) {
-            mas[v] = l<n ? vals[l] : neutral;
-            def[v] = neutralUpdate;
-            return;
-        }
+        if (l==r) {mas[v] = l<n ? vals[l] : neutral; def[v] = neutralUpdate; return;}
         int m = (l+r)/2;
-        build(vals, n, 2*v, l, m);
-        build(vals, n, 2*v+1, m+1, r);
+        build(vals, n, 2*v, l, m); build(vals, n, 2*v+1, m+1, r);
         mas[v] = rqOp(applyUpdate(mas[2*v], def[2*v]), applyUpdate(mas[2*v+1], def[2*v+1]));
         def[v] = neutralUpdate;
     }
     void push(int v) { /*push(Node*&)*/
         def[2*v] = combineUpdates(def[2*v], def[v]);
-        def[2*v+1] = combineUpdates(def[2*v+1], def[v]);
-        def[v] = neutralUpdate;
+        def[2*v+1] = combineUpdates(def[2*v+1], def[v]); def[v] = neutralUpdate;
     }
-
     void update(int rq_l, int rq_r, val_t val, int v = 1, int l = 0, int r = size-1) {
         if (l > rq_r || r < rq_l) return;
-        if (rq_l <= l && r <= rq_r) {
-            def[v] = combineUpdates(def[v], val);
-            return;
-        }
-        push(v);
-        int m = (l+r)/2;
-        update(rq_l, rq_r, val, 2*v, l, m);
-        update(rq_l, rq_r, val, 2*v+1, m+1, r);
+        if (rq_l <= l && r <= rq_r) {def[v] = combineUpdates(def[v], val); return;}
+        push(v); int m = (l+r)/2;
+        update(rq_l, rq_r, val, 2*v, l, m); update(rq_l, rq_r, val, 2*v+1, m+1, r);
         mas[v] = rqOp(applyUpdate(mas[2*v], def[2*v]), applyUpdate(mas[2*v+1], def[2*v+1]));
     }
-
     /*pair<val_t, Node*>*/val_t query(int rq_l, int rq_r, int v = 1, int l = 0, int r = size-1) {
         if (l > rq_r || r < rq_l) return neutral;
-        if (rq_l <= l && r <= rq_r) {
-            return applyUpdate(mas[v], def[v]);
-        }
-        push(v);
-        int m = (l+r)/2;
+        if (rq_l <= l && r <= rq_r) return applyUpdate(mas[v], def[v]);
+        push(v); int m = (l+r)/2;
         auto ans = rqOp(query(rq_l, rq_r, 2*v, l, m), query(rq_l, rq_r, 2*v+1, m+1, r));
         mas[v] = rqOp(applyUpdate(mas[2*v], def[2*v]), applyUpdate(mas[2*v+1], def[2*v+1]));
         return ans;
@@ -132,3 +115,29 @@ struct segtree {
 //LiChao tree: store line in vert. On process vert: choose best (min/max) among newF and mas[v] in pnt m
 //Based on comparison in pnt l, in one half newF never could be optimal, descend to other
 //Leafs trivially updated. Answer to query: best in given point on path to leaf
+struct segtree {//persistent (WIHTOUT group ops, for them adopt upper version (THINK 150 TIMES!!!))
+    static val_t rqOp(val_t lhs, val_t rhs) {return lhs+rhs;}
+    struct Node {
+        Node *l, *r; val_t val; Node(): l(nullptr), r(nullptr), val(0) {};
+        Node(Node *l, Node *r): l(l), r(r) { val = rqOp((l ? l->val : neutral), (r? r->val : neutral));}
+    };
+	
+    Node* build(int l = 0, int r = size-1) {
+        if (l==r) return new Node();
+        int m = (l+r)/2; return new Node(build(l, m), build(m+1, r));
+    }
+    Node* modify(int pos, int delta, Node* v, int l  = 0, int r = size-1) {
+        if (l==r) {auto tmp = new Node(*v); tmp->val = v->val + delta; return tmp;}
+        int m = (l+r)/2;
+        auto lCh = pos<=m ? modify(pos, delta, v->l, l, m) : v->l;
+        auto rCh = pos>m ? modify(pos, delta, v->r, m+1, r) : v->r;
+        return new Node(lCh, rCh);
+    }
+    val_t query(int rq_l, int rq_r, Node* v, int l = 0, int r = size-1) {
+        if (l > rq_r || r < rq_l) return 0;
+        if (rq_l <=l && r<=rq_r) return v->val;
+        int m = (l+r)/2;
+        val_t res = rqOp(query(rq_l, rq_r, v->l, l, m), query(rq_l, rq_r, v->r, m+1, r));
+        return res;
+    }
+};
