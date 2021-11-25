dc4d77e32d98170a0333646d5e3e20000fad2dbd
Anton Mokrousov <settingx@mail.ru>
2021-11-25 23:21:01 +0700

    Remove efficient range tree with group add

--- range_tree.cpp
+++ range_tree.cpp
@@ -23,43 +23,6 @@ struct rmq_t { // ********* RMQ without interval modification *********
 		return ans;
 	}
 };
-//if query op is addition, we need store len[v] to scale impact of stored addition
-struct rmq_rm_t { // ***** RMQ with interval modification support *****
-	val_t ans[size << 1], add[size << 1];
-	rmq_rm_t() { }
-	rmq_rm_t(val_t *a) {
-		memset(add, 0, sizeof(add)); // ! you may need something else here
-		memcpy(ans + size, a, sizeof(val_t) * size);
-		for (int i = size - 1; i > 0; --i) ans[i] = min(ans[i << 1], ans[(i << 1) + 1]);
-	}
-	void modify(int l, int r, val_t val) {
-		if (l > r) return;
-		l += size; r += size;
-		add[l] += val; if (l < r) add[r] += val;
-		while (l > 0) {
-			if ((l & 1) == 0 && (l + 1) < r) add[l + 1] += val;
-			if ((r & 1) == 1 && (r - 1) > l) add[r - 1] += val;
-			l >>= 1; r >>= 1;
-			ans[l] = min(ans[l << 1] + add[l << 1], ans[(l << 1) + 1] + add[(l << 1) + 1]);
-			ans[r] = min(ans[r << 1] + add[r << 1], ans[(r << 1) + 1] + add[(r << 1) + 1]);
-		}
-	}
-	val_t query(int l, int r) {
-		if (l > r) return inf;
-		l += size; r += size;
-		val_t lans = ans[l] + add[l], rans = ans[r] + add[r];
-		//ll left = len[l], right = l<r ? len[r] : 0;
-		while (l > 0) {
-			if ((l & 1) == 0 && (l + 1) < r) lans = min(lans, ans[l + 1] + add[l + 1]);
-			//if ((l & 1) == 0 && (l + 1) < r) left += len[l+1];
-			if ((r & 1) == 1 && (r - 1) > l) rans = min(rans, ans[r - 1] + add[r - 1]);
-			//if ((r & 1) == 1 && (r - 1) > l) right += len[r-1];
-			l >>= 1; r >>= 1;
-			lans += add[l]; rans += add[r]; //lans += add[l] * left; rans += add[r] * right;
-		}
-		return min(lans, rans);
-	}
-};
 // **************************** Fenvik tree ****************************
 struct fenvic_t { //for multiple dimensions loops by each index
     val_t mas[size];
@@ -111,6 +74,7 @@ struct segtree {
         mas[v] = rqOp(applyUpdate(mas[2*v], def[2*v]), applyUpdate(mas[2*v+1], def[2*v+1]));
         return ans;
     }
+    //TODO: add bin search (from left)
 };
 //LiChao tree: store line in vert. On process vert: choose best (min/max) among newF and mas[v] in pnt m
 //Based on comparison in pnt l, in one half newF never could be optimal, descend to other
