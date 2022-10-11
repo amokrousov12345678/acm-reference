3093c56fa77a808926e90cbcc6153956fa209d1a
Anton Mokrousov <settingx@mail.ru>
2022-10-11 23:57:01 +0700

    Compress mincost-maxflow

--- min_cost_max_flow.cpp
+++ min_cost_max_flow.cpp
@@ -7,19 +7,10 @@ int cnt, n;//IN cnt - cnt edges. Set to 0 before use on other graph, IN n - cnt 
 vector<int> g[maxn];//IN: g[v] - IDS of edges go from v
 
 void add_edge(int u, int v, int c, dist_t w) {
-	edges[cnt].dest = v;
-	edges[cnt].back = cnt + 1;
-	edges[cnt].f = 0;
-	edges[cnt].c = c;
-	edges[cnt].w = w;
-	g[u].push_back(cnt);
-	edges[cnt + 1].dest = u;
-	edges[cnt + 1].back = cnt;
-	edges[cnt + 1].f = 0;
-	edges[cnt + 1].c = 0;
-	edges[cnt + 1].w = -w;
-	g[v].push_back(cnt + 1);
-	cnt += 2;
+	edges[cnt].dest = v; edges[cnt].back = cnt + 1;
+	edges[cnt].f = 0; edges[cnt].c = c; edges[cnt].w = w; g[u].push_back(cnt);
+	edges[cnt + 1].dest = u; edges[cnt + 1].back = cnt; edges[cnt + 1].f = 0;
+	edges[cnt + 1].c = 0; edges[cnt + 1].w = -w; g[v].push_back(cnt + 1); cnt += 2;
 }
 int was[maxn], prevE[maxn];//was[v] - if v visited, prevE[v] - id of backedge to v
 flow_t r[maxn]; dist_t dist[maxn], phi[maxn];//r[v] - min cap of path to v
@@ -27,10 +18,8 @@ flow_t r[maxn]; dist_t dist[maxn], phi[maxn];//r[v] - min cap of path to v
 bool dijk(int s, int t) {
 	int i, j;
 	for (i = 0; i < n; ++i) dist[i] = inf;
-	memset(was, 0, sizeof(int) * n);
-	memset(r, 0, sizeof(int) * n);
-	dist[s] = 0;
-	r[s] = inf;
+	memset(was, 0, sizeof(int) * n); memset(r, 0, sizeof(int) * n);
+	dist[s] = 0; r[s] = inf;
 	for (i = 0; i < n; ++i) {
 		int mv = -1;
 		for (j = 0; j < n; ++j) if (!was[j] && (mv == -1 || dist[mv] > dist[j])) mv = j;
@@ -40,16 +29,14 @@ bool dijk(int s, int t) {
 			edge &e = edges[ g[mv][j] ];
 			if (!was[e.dest] && e.f < e.c && dist[e.dest] > dist[mv] + e.w + phi[mv] - phi[e.dest]) {
 				dist[e.dest] = dist[mv] + e.w + phi[mv] - phi[e.dest];
-				r[e.dest] = min(r[mv], e.c - e.f);
-				prevE[e.dest] = e.back;
+				r[e.dest] = min(r[mv], e.c - e.f); prevE[e.dest] = e.back;
 			}
 		}
 	}
 	return r[t] > 0;/*r[t] >= eps*/
 }
 dist_t aug(int s, int t) {
-	flow_t rr = r[t];
-	dist_t ans = 0;
+	flow_t rr = r[t]; dist_t ans = 0;
 	while (s != t) {
 		edge &e = edges[prevE[t]]; ans -= e.w * rr; e.f -= rr; edges[e.back].f += rr; t = e.dest;
 	}
@@ -58,10 +45,7 @@ dist_t aug(int s, int t) {
 
 flow_t flow; dist_t cost;//OUT flow - max flow, cost - sum cost
 void min_cost_max_flow(int s, int t) {
-	int i; for (i = 0; i < n; ++i) phi[i] = 0;
-	for (i = 0; i < cnt; ++i) edges[i].f = 0;
-	cost = flow = 0;
-	while (dijk(s, t)) {
-		flow += r[t]; cost += aug(s, t); for (i = 0; i < n; ++i) phi[i] += dist[i];
-	}
+	cost = flow = 0; int i; for (i = 0; i < n; ++i) phi[i] = 0; 
+	for (i = 0; i < cnt; ++i) edges[i].f = 0; 
+	while (dijk(s, t)) { flow += r[t]; cost += aug(s, t); for (i = 0; i < n; ++i) phi[i] += dist[i];}
 }
